1865da9774d9da6e8c0c959973c4950b38bb0066
Dhairya <dchoudhry999@gmail.com>
2026-03-19 11:45:37 +0530

    Deployment ready app

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from utils import *
+import os
 
 app = Flask(__name__)
 
@@ -42,5 +43,6 @@ def analyze():
         recommended_roles=recommended_roles
     )
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
